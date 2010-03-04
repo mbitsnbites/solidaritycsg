002af272060a59fa0373a5668ec8757ea90bdb46
solidaritycsg <solidaritycsg@8b5ce176-d626-451d-8618-847bdeea156b>
2010-03-04 08:14:26 +0000

    Started working on a couple of new binary tree classes for fast mesh intersection routines.

--- trunk/source/Voxelize.h
+++ trunk/source/Voxelize.h
@@ -45,9 +45,91 @@ class Triangle {
 
   private:
     Vector3 * mVertices[3]; ///< Pointers to the actual vertex coordinates.
-    Vector3 mMin;           ///< Lower bound of the triangle AABB.
-    Vector3 mMax;           ///< Upper bound of the triangle AABB.
     Vector3 mNormal;        ///< Triangle normal (out direction).
+
+    friend class XYTreeNode;
+    friend class ZTreeNode;
+};
+
+/// Generic binary tree node class.
+class TreeNode {
+  public:
+    /// Check if this is a leaf node.
+    inline bool IsLeafNode()
+    {
+      return (mPtr2 == 0);
+    }
+
+    /// Get the leaf object. If this is a parent node, return zero.
+    inline Triangle * LeafItem()
+    {
+      if(IsLeafNode())
+        return (Triangle *) mPtr1;
+      else
+        return (Triangle *) 0;
+    }
+
+    /// Get child A. If this is a leaf node, return zero.
+    inline TreeNode * ChildA()
+    {
+      if(IsLeafNode())
+        return (TreeNode *) 0;
+      else
+        return (TreeNode *) mPtr1;
+    }
+
+    /// Get child B. If this is a leaf node, return zero.
+    inline TreeNode * ChildB()
+    {
+      return (TreeNode *) mPtr2;
+    }
+
+  private:
+    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
+    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
+};
+
+/// Bounding rectangle tree node class.
+class XYTreeNode : public TreeNode {
+  public:
+    /// Constructor for leaf nodes.
+    XYTreeNode(Triangle * aTriangle);
+
+    /// Constructor for parent nodes.
+    XYTreeNode(XYTreeNode * aChildA, XYTreeNode * aChildB);
+
+    inline bool PointInside(Vector3 &aPoint)
+    {
+      return ((aPoint.x > mMin[0]) && (aPoint.x < mMax[0]) &&
+              (aPoint.y > mMin[1]) && (aPoint.y < mMax[1]));
+    }
+
+  private:
+    double mMin[2]; ///< Lower bound
+    double mMax[2]; ///< Upper bound
+    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
+    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
+};
+
+/// Bounding interval tree node class.
+class ZTreeNode : public TreeNode {
+  public:
+    /// Constructor for leaf nodes.
+    ZTreeNode(Triangle * aTriangle);
+
+    /// Constructor for parent nodes.
+    ZTreeNode(ZTreeNode * aChildA, ZTreeNode * aChildB);
+
+    inline bool PointInside(Vector3 &aPoint)
+    {
+      return ((aPoint.z > mMinZ) && (aPoint.z < mMaxZ));
+    }
+
+  private:
+    double mMinZ;   ///< Lower bound
+    double mMaxZ;   ///< Upper bound
+    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
+    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
 };
 
 /// Voxelize class.
