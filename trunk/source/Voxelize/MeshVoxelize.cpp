a3143ace5b978ebbd43970308e7f56fe65eb32b5
solidaritycsg <solidaritycsg@8b5ce176-d626-451d-8618-847bdeea156b>
2010-03-04 11:00:20 +0000

    Split the Voxelize class into a parent class, Voxelize, and a child class, MeshVoxelize.

---
+++ trunk/source/Voxelize/MeshVoxelize.cpp
@@ -0,0 +1,256 @@
+/*
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "MeshVoxelize.h"
+
+using namespace std;
+
+namespace csg {
+
+//-----------------------------------------------------------------------------
+// Triangle
+//-----------------------------------------------------------------------------
+
+void Triangle::SetCoordinates(Vector3 * p1, Vector3 * p2, Vector3 * p3)
+{
+  // Assign vertex coordinate pointers
+  mVertices[0] = p1;
+  mVertices[1] = p2;
+  mVertices[2] = p3;
+
+  // Calculate triangle normal (normalized cross product)
+  Vector3 v1 = *p2 - *p1;
+  Vector3 v2 = *p3 - *p1;
+  mNormal.x = v1.y * v2.z - v1.z * v2.y;
+  mNormal.y = v1.z * v2.x - v1.x * v2.z;
+  mNormal.z = v1.x * v2.y - v1.y * v2.x;
+  mNormal = mNormal.Normalize();
+}
+
+bool Triangle::IntersectPlane(double aPlaneZ, Vector3 * p1, Vector3 * p2)
+{
+  double d[3], s;
+  d[0] = mVertices[0]->z - aPlaneZ;
+  d[1] = mVertices[1]->z - aPlaneZ;
+  d[2] = mVertices[2]->z - aPlaneZ;
+
+  if((d[0] * d[1]) < 0.0)
+  {
+    s = d[0] / (mVertices[1]->z - mVertices[0]->z);
+    if((s < 0.0) || (s > 1.0))
+      return false;
+    *p1 = (*mVertices[1] - *mVertices[0]) * s + *mVertices[0];
+    if((d[1] * d[2]) < 0.0)
+    {
+      s = d[1] / (mVertices[2]->z - mVertices[1]->z);
+      if((s < 0.0) || (s > 1.0))
+        return false;
+      *p2 = (*mVertices[2] - *mVertices[1]) * s + *mVertices[1];
+    }
+    else
+    {
+      s = d[2] / (mVertices[0]->z - mVertices[2]->z);
+      if((s < 0.0) || (s > 1.0))
+        return false;
+      *p2 = (*mVertices[0] - *mVertices[2]) * s + *mVertices[2];
+    }
+  }
+  else
+  {
+    s = d[1] / (mVertices[2]->z - mVertices[1]->z);
+    if((s < 0.0) || (s > 1.0))
+      return false;
+    *p1 = (*mVertices[2] - *mVertices[1]) * s + *mVertices[1];
+    s = d[2] / (mVertices[0]->z - mVertices[2]->z);
+    if((s < 0.0) || (s > 1.0))
+      return false;
+    *p2 = (*mVertices[0] - *mVertices[2]) * s + *mVertices[2];
+  }
+
+  return true;
+}
+
+double Triangle::IntersectZRay(Vector3 aOrigin)
+{
+  // Is the triangle parallel to the ray?
+  if(fabs(mNormal.z) < 1e-50)
+    return -1.0;
+
+  Vector3 w0 = aOrigin - *mVertices[0];
+
+  // Is the point inside the triangle (use 2D parametric coordinates)?
+  // NOTE: This is a 2D version of the test in:
+  //  http://softsurfer.com/Archive/algorithm_0105/algorithm_0105.htm
+  double u[2], v[2];
+  u[0] = mVertices[1]->x - mVertices[0]->x;
+  u[1] = mVertices[1]->y - mVertices[0]->y;
+  v[0] = mVertices[2]->x - mVertices[0]->x;
+  v[1] = mVertices[2]->y - mVertices[0]->y;
+  double uu = u[0] * u[0] + u[1] * u[1];
+  double uv = u[0] * v[0] + u[1] * v[1];
+  double vv = v[0] * v[0] + v[1] * v[1];
+  double wu = w0.x * u[0] + w0.y * u[1];
+  double wv = w0.x * v[0] + w0.y * v[1];
+  double D = 1.0 / (uv * uv - uu * vv);
+  double s = (uv * wv - vv * wu) * D;
+  if((s < 0.0) || (s > 1.0))
+    return -1.0;
+  double t = (uv * wu - uu * wv) * D;
+  if((t < 0.0) || ((s + t) > 1.0))
+    return -1.0;
+
+  // NOTE: As an alternative method, check the winding number, see:
+  //  http://softsurfer.com/Archive/algorithm_0103/algorithm_0103.htm
+
+  // Get intersection offset
+  return -(mNormal.x * w0.x + mNormal.y * w0.y) / mNormal.z - w0.z;
+}
+
+
+//-----------------------------------------------------------------------------
+// XYTreeNode
+//-----------------------------------------------------------------------------
+
+XYTreeNode::XYTreeNode(Triangle * aTriangle)
+{
+  // Set pointers
+  mPtr1 = (void *) aTriangle;
+  mPtr2 = 0;
+
+  // Calculate leaf node bounding rectangle
+  mMin[0] = mMax[0] = aTriangle->mVertices[0]->x;
+  mMin[1] = mMax[1] = aTriangle->mVertices[0]->y;
+  for(int i = 1; i <= 2; ++ i)
+  {
+    if(aTriangle->mVertices[i]->x < mMin[0])
+      mMin[0] = aTriangle->mVertices[i]->x;
+    else if(aTriangle->mVertices[i]->x > mMax[0])
+      mMax[0] = aTriangle->mVertices[i]->x;
+    if(aTriangle->mVertices[i]->y < mMin[1])
+      mMin[1] = aTriangle->mVertices[i]->y;
+    else if(aTriangle->mVertices[i]->y > mMax[1])
+      mMax[1] = aTriangle->mVertices[i]->y;
+  }
+}
+
+XYTreeNode::XYTreeNode(XYTreeNode * aChildA, XYTreeNode * aChildB)
+{
+  // Set pointers
+  mPtr1 = (void *) aChildA;
+  mPtr2 = (void *) aChildB;
+
+  // Calculate node bounding rectangle
+  for(int i = 0; i < 2; ++ i)
+  {
+    if(aChildA->mMin[i] < aChildB->mMin[i])
+      mMin[i] = aChildA->mMin[i];
+    else
+      mMin[i] = aChildB->mMin[i];
+    if(aChildA->mMax[i] > aChildB->mMax[i])
+      mMax[i] = aChildA->mMax[i];
+    else
+      mMax[i] = aChildB->mMax[i];
+  }
+}
+
+//-----------------------------------------------------------------------------
+// ZTreeNode
+//-----------------------------------------------------------------------------
+
+ZTreeNode::ZTreeNode(Triangle * aTriangle)
+{
+  // Set pointers
+  mPtr1 = (void *) aTriangle;
+  mPtr2 = 0;
+
+  // Calculate leaf node bounding interval
+  mMinZ = mMaxZ = aTriangle->mVertices[0]->z;
+  for(int i = 1; i <= 2; ++ i)
+  {
+    if(aTriangle->mVertices[i]->z < mMinZ)
+      mMinZ = aTriangle->mVertices[i]->z;
+    else if(aTriangle->mVertices[i]->z > mMaxZ)
+      mMaxZ = aTriangle->mVertices[i]->z;
+  }
+}
+
+ZTreeNode::ZTreeNode(ZTreeNode * aChildA, ZTreeNode * aChildB)
+{
+  // Set pointers
+  mPtr1 = (void *) aChildA;
+  mPtr2 = (void *) aChildB;
+
+  // Calculate node bounding interval
+  if(aChildA->mMinZ < aChildB->mMinZ)
+    mMinZ = aChildA->mMinZ;
+  else
+    mMinZ = aChildB->mMinZ;
+  if(aChildA->mMaxZ > aChildB->mMaxZ)
+    mMaxZ = aChildA->mMaxZ;
+  else
+    mMaxZ = aChildB->mMaxZ;
+}
+
+
+//-----------------------------------------------------------------------------
+// MeshVoxelize
+//-----------------------------------------------------------------------------
+
+MeshVoxelize::MeshVoxelize()
+{
+  // FIXME!
+}
+
+MeshVoxelize::~MeshVoxelize()
+{
+  // FIXME!
+}
+
+void MeshVoxelize::SetTriangles(int aTriangleCount, int * aIndices,
+  int aVertexCount, float * aVertices)
+{
+  // Create vertex array
+  mVertices.resize(aVertexCount);
+  float * vPtr = aVertices;
+  for(int i = 0; i < aVertexCount; ++ i)
+  {
+    mVertices[i] = Vector3(vPtr[0], vPtr[1], vPtr[2]);
+    vPtr += 3;
+  }
+
+  // Create triangle array
+  mTriangles.resize(aTriangleCount);
+  int * iPtr = aIndices;
+  for(int i = 0; i < aVertexCount; ++ i)
+  {
+    mTriangles[i].SetCoordinates(&mVertices[iPtr[0]],
+                                 &mVertices[iPtr[1]],
+                                 &mVertices[iPtr[2]]);
+    iPtr += 3;
+  }
+
+  // Build 2D bounding rectangle tree (XY)
+  // FIXME!
+
+  // Build 1D bounding interval tree (Z)
+  // FIXME!
+}
+
+void MeshVoxelize::CalculateSlice(Voxel * aSlice, int aZ)
+{
+  // FIXME!
+}
+
+}
--- trunk/source/Voxelize/Voxelize.h
+++ trunk/source/Voxelize/Voxelize.h
@@ -16,121 +16,16 @@
 #ifndef _VOXELIZE_H_
 #define _VOXELIZE_H_
 
-#include <vector>
 #include "../Vector3.h"
 
 namespace csg {
 
-/// The Voxel primitive is an 8 bit unsigned integer.
-typedef unsigned char Voxel;
+/// The Voxel primitive is an 8 bit signed integer.
+typedef signed char Voxel;
 
-/// Triangle class.
-class Triangle {
-  public:
-    /// Define the triangle. p1, p2 and p3 are pointers to the vertex
-    /// coordinates for the three corners of the triangle.
-    void SetCoordinates(Vector3 * p1, Vector3 * p2, Vector3 * p3);
+/// Maximum voxel value
+#define VOXEL_MAX 128
 
-    /// Calculate the intersection with an XY-plane.
-    /// The result of the intersection, if any, is a line segment that is
-    /// returned in p1 and p2.
-    bool IntersectPlane(double aPlaneZ, Vector3 * p1, Vector3 * p2);
-
-    /// Calculate the intersection with a Z-ray.
-    /// Only positive intersections are considered.
-    /// The result of the intersection is the t-value (offset) along the ray for
-    /// the intersection. A negative return value indicates that there was no
-    /// intersection.
-    double IntersectZRay(Vector3 aOrigin);
-
-  private:
-    Vector3 * mVertices[3]; ///< Pointers to the actual vertex coordinates.
-    Vector3 mNormal;        ///< Triangle normal (out direction).
-
-    friend class XYTreeNode;
-    friend class ZTreeNode;
-};
-
-/// Generic binary tree node class.
-class TreeNode {
-  public:
-    /// Check if this is a leaf node.
-    inline bool IsLeafNode()
-    {
-      return (mPtr2 == 0);
-    }
-
-    /// Get the leaf object. If this is a parent node, return zero.
-    inline Triangle * LeafItem()
-    {
-      if(IsLeafNode())
-        return (Triangle *) mPtr1;
-      else
-        return (Triangle *) 0;
-    }
-
-    /// Get child A. If this is a leaf node, return zero.
-    inline TreeNode * ChildA()
-    {
-      if(IsLeafNode())
-        return (TreeNode *) 0;
-      else
-        return (TreeNode *) mPtr1;
-    }
-
-    /// Get child B. If this is a leaf node, return zero.
-    inline TreeNode * ChildB()
-    {
-      return (TreeNode *) mPtr2;
-    }
-
-  private:
-    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
-    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
-};
-
-/// Bounding rectangle tree node class.
-class XYTreeNode : public TreeNode {
-  public:
-    /// Constructor for leaf nodes.
-    XYTreeNode(Triangle * aTriangle);
-
-    /// Constructor for parent nodes.
-    XYTreeNode(XYTreeNode * aChildA, XYTreeNode * aChildB);
-
-    inline bool PointInside(Vector3 &aPoint)
-    {
-      return ((aPoint.x > mMin[0]) && (aPoint.x < mMax[0]) &&
-              (aPoint.y > mMin[1]) && (aPoint.y < mMax[1]));
-    }
-
-  private:
-    double mMin[2]; ///< Lower bound
-    double mMax[2]; ///< Upper bound
-    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
-    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
-};
-
-/// Bounding interval tree node class.
-class ZTreeNode : public TreeNode {
-  public:
-    /// Constructor for leaf nodes.
-    ZTreeNode(Triangle * aTriangle);
-
-    /// Constructor for parent nodes.
-    ZTreeNode(ZTreeNode * aChildA, ZTreeNode * aChildB);
-
-    inline bool PointInside(Vector3 &aPoint)
-    {
-      return ((aPoint.z > mMinZ) && (aPoint.z < mMaxZ));
-    }
-
-  private:
-    double mMinZ;   ///< Lower bound
-    double mMaxZ;   ///< Upper bound
-    void * mPtr1;   ///< Pointer to child A (mPtr2!=0) or a Triangle object (mPtr2==0)
-    void * mPtr2;   ///< Pointer to child B, or zero (for leaf nodes)
-};
 
 /// Voxelize class.
 class Voxelize {
@@ -139,14 +34,7 @@ class Voxelize {
     Voxelize();
 
     /// Destructor.
-    ~Voxelize();
-
-    /// Define the triangle surface to be voxelized.
-    /// A copy of the triangle surface is made internally in the Voxelize
-    /// object, so the data arrays (aIndices, aVertices) can be free:d once this
-    /// function has been called.
-    void SetTriangles(int aTriangleCount, int * aIndices, int aVertexCount,
-                      float * aVertices);
+    virtual ~Voxelize() {}
 
     /// Define the voxel space boundaries and resolution.
     void SetVoxelSpace(float aMinX, float aMinY, float aMinZ,
@@ -155,18 +43,12 @@ class Voxelize {
 
     /// Calculate a single slice of the voxel volume.
     /// The slice must be allocated by the caller, and hold DivX * DivY voxels.
-    void CalculateSlice(Voxel * aSlice, int aZ);
+    virtual void CalculateSlice(Voxel * aSlice, int aZ) = 0;
 
   private:
     Vector3 mMin; ///< Lower bound of the voxel space bounding box.
     Vector3 mMax; ///< Upper bound of the voxel space bounding box.
     int mDiv[3];  ///< Number of divisions of the voxel space (x, y, z).
-
-    /// Triangles.
-    std::vector<Triangle> mTriangles;
-
-    /// Vertex coordinates.
-    std::vector<Vector3> mVertices;
 };
 
 }
