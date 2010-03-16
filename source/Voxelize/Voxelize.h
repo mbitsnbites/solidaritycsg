e0ab1b92226134a74b94d0e02df0c0d2768344f3
solidaritycsg <solidaritycsg@8b5ce176-d626-451d-8618-847bdeea156b>
2010-03-16 15:33:30 +0000

    Moved the SampleSpace class into its own unit.

--- source/Voxelize/Voxelize.h
+++ source/Voxelize/Voxelize.h
@@ -20,53 +20,9 @@
 
 #include "../Vector3.h"
 #include "../BoundingBox.h"
+#include "../SampleSpace.h"
 
 namespace csg {
-
-/// The Voxel primitive is an 8 bit signed integer.
-typedef signed char Voxel;
-
-/// Maximum voxel value
-#define VOXEL_MAX 127
-
-/// Reserved voxel value for "UNVISITED"
-#define VOXEL_UNVISITED -128
-
-/// Voxel sampler space definition class.
-class SampleSpace {
-  public:
-    /// Sanity check of the sample space parameters.
-    inline bool IsValid()
-    {
-      return ((mDiv[0] >= 1) && (mDiv[1] >= 1) && (mDiv[2] >= 1) &&
-              (mAABB.mMax.x > mAABB.mMin.x) && (mAABB.mMax.y > mAABB.mMin.y) &&
-              (mAABB.mMax.z > mAABB.mMin.z));
-    }
-
-    /// Return the voxel spacing (voxel size).
-    inline Vector3 VoxelSize()
-    {
-      Vector3 d = mAABB.Diagonal();
-      d.x /= mDiv[0];
-      d.y /= mDiv[1];
-      d.z /= mDiv[2];
-      return d;
-    }
-
-    /// Define the sample space. This method calculates the optimal sample
-    /// space parameters, based on the given minimal enclosing bounding box
-    /// and the desired resolution.
-    void DefineSpace(BoundingBox &aAABB, Vector3 aResolution);
-
-    /// Define the sample space (uniform resolution).
-    inline void DefineSpace(BoundingBox &aAABB, double aResolution)
-    {
-      DefineSpace(aAABB, Vector3(aResolution, aResolution, aResolution));
-    }
-
-    BoundingBox mAABB; ///< Bonding box for the voxel space.
-    int mDiv[3];       ///< Number of divisions of the voxel space (x, y, z).
-};
 
 /// Voxelize class.
 class Voxelize {
